885c693d77125659ba2925ef9cc97e6e8eef06cb
Wojciech Czarny <Wojciech.Czarny@sabre.com>
2014-08-24 13:01:28 +0200

    Jump animation final

---
+++ project/Assets/ChangeColor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+	
+	public GameObject[] children;
+	private SpriteRenderer[] childrensSprites;
+
+	// Use this for initialization
+	void Start () {
+		childrensSprites = new SpriteRenderer[children.Length];
+		int i = 0;
+		foreach(GameObject go in children)
+			childrensSprites[i++] = go.GetComponent<SpriteRenderer>();
+	}
+
+	void Change(Color color) {
+		foreach (SpriteRenderer sr in childrensSprites)
+			sr.color = color;
+		Debug.Log (color);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
